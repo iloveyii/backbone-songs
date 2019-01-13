bfc4eff8ff0a36f6cff16099f1765771583bb4c0
Hazrat Ali <iloveyii@yahoo.com>
2019-01-13 21:46:20 +0100

    listen on 0000

--- app.js
+++ app.js
@@ -21,5 +21,5 @@ app.get('/', function (req, res) {
 // handle api call here using controllers
 songsController(app);
 
-app.listen(7000);
-console.log('Express is listening on port: 7000');
+app.listen(7000, '0.0.0.0');
+console.log('Express is listening on port: 0000');
